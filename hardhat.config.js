a5d84672a26363a72c2159040536e3e269a4062c
Nebojsa Konsta <nebojsa.konsta@gmail.com>
2024-11-12 15:35:27 +0100

    optimization: 5525 gas using runs in config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.27"
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000000,
+      },
+    },
   },
 };
