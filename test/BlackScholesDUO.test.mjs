a8e85f3a01cebbd44ba571bad5cc9dff0c32b718
Nebojsa Konsta <nebojsa.konsta@gmail.com>
2025-02-27 10:01:53 +0100

    sqrt [1, 100): gas 352, 583

--- test/BlackScholesDUO.test.mjs
+++ test/BlackScholesDUO.test.mjs
@@ -741,16 +741,16 @@ describe("BlackScholesDUO (SOL and JS)", function () {
 
     describe.only("sqrt", function () {
       // todo: test all limits like 1.04427
-      it("sqrt [1, 1.04427]", async function () {
+      it("sqrt [1, 1.0746]", async function () { // root(64, 100) = 1.074607828321317497
         let totalGas = 0, count = 0;
-        for (let x = 1; x < 1.0442737824274138403; x += 0.0001) {
+        for (let x = 1; x < 1.074607828321317497; x += 0.0001) {
           const expected = Math.sqrt(x);
           const actualJS = blackScholesNUMJS.sqrt(x);
           const absError = Math.abs(actualJS - expected);
           const relError = expected !== 0 ? absError / expected * 100 : 0;
           // console.log("Rel error for x: ", x.toFixed(4), "JS:", relError.toFixed(12) + "%, ", "act: " + actualJS.toFixed(12), "exp: " + expected.toFixed(12));
           // assert.isBelow(absError, 0.00000001);
-          assert.isBelow(relError, 0.000000000001); // 1e-12 
+          assert.isBelow(relError, 0.000000000072); // 1e-12 
 
           if (duoTest) {
             const { blackScholesNUM } = duoTest ? await loadFixture(deployNUM) : { blackScholesNUM: null };
@@ -760,7 +760,7 @@ describe("BlackScholesDUO (SOL and JS)", function () {
             const relError = expected !== 0 ? absError / expected * 100 : 0;
             // console.log("Rel error for x: ", x.toFixed(4), "SOL:", relError.toFixed(12) + "%,", "act: " + actualSOL.toFixed(12), "exp: " + expected.toFixed(12));
             // assert.isBelow(absError, 0.00000001);
-            assert.isBelow(relError, 0.000000000020); // 1e-12 
+            assert.isBelow(relError, 0.000000000072); // 1e-12 
 
             totalGas += parseInt(await blackScholesNUM.sqrtMeasureGas(tokens(x)));
             count++;
@@ -771,7 +771,7 @@ describe("BlackScholesDUO (SOL and JS)", function () {
 
       it("sqrt [1.04427, 2)", async function () {
         let totalGas = 0, count = 0;
-        for (let x = 1.0442737824274138403; x < 2; x += 0.001) {
+        for (let x = 1.074607828321317497; x < 100; x += 0.1) {
           const expected = Math.sqrt(x);
           // const actualJS = blackScholesNUMJS.sqrt(x);
           // const absError = Math.abs(actualJS - expected);
@@ -786,9 +786,9 @@ describe("BlackScholesDUO (SOL and JS)", function () {
             const actualSOL = (await blackScholesNUM.sqrt(tokens(x))).toString() / 1e18;
             const absError = Math.abs(actualSOL - expected);
             const relError = expected !== 0 ? absError / expected * 100 : 0;
-            // console.log("Rel error for x: ", x.toFixed(4), "SOL:", relError.toFixed(12) + "%,", "act: " + actualSOL.toFixed(12), "exp: " + expected.toFixed(12));
+            console.log("Rel error for x: ", x.toFixed(4), "SOL:", relError.toFixed(12) + "%,", "act: " + actualSOL.toFixed(12), "exp: " + expected.toFixed(12));
             // assert.isBelow(absError, 0.00000001);
-            assert.isBelow(relError, 0.000000000020); // 1e-12 
+            assert.isBelow(relError, 0.000000000072); // 1e-12 
 
             totalGas += parseInt(await blackScholesNUM.sqrtMeasureGas(tokens(x)));
             count++;
