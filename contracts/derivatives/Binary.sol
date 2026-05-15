68c46d2816f54e9d18748740ce9936e75d044cd0
Nebojsa Konsta <nebojsa.konsta@gmail.com>
2026-05-15 09:33:23 +0200

    Solidity version compatibility down to 0.8.31

--- contracts/derivatives/Binary.sol
+++ contracts/derivatives/Binary.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.34;
+pragma solidity ^0.8.31;
 
 import "../math/Math.sol";
 
--- contracts/math/Math.sol
+++ contracts/math/Math.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.34;
+pragma solidity ^0.8.31;
 
 /// @title DeFiMath: High-precision Math Library for Solidity
 /// @author DeFiMath (https://defimath.com)
